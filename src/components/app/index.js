3959906a122f98611d9a5b5ae1ddbbd12d86087e
Anton <anton26kur@gmail.com>
2020-04-23 16:04:34 +0300

    initial

---
+++ src/components/app/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import './app.sass'
+
+export default class App extends Component {
+  render() {
+    return(
+      <div styleName='app'>Hello</div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const js = {
+  test: /\.js$/,
+  use: 'babel-loader'
+}
+
+const sass = {
+  test: /\.sass$/,
+  use: [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+        modules: {
+          localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
+        }
+      }
+    },
+    'sass-loader'
+  ]
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    port: 8000
+  },
+  module: {
+    rules: [js, sass]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'dist/index.html')
+    })
+  ]
+}
